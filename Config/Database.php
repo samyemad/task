2718ad8f27cf2956deaa457968a4c87d97d4b79f
samy <samyemad4@gmail.com>
2021-06-14 13:39:48 +0200

    feature/add_config_directory_that_contains_connection_informations

---
+++ Config/Database.php
@@ -0,0 +1,11 @@
+<?php
+namespace Config;
+
+class Database
+{
+    const SERVERNAME = "localhost";
+    const USERNAME = "root";
+    const PASSWORD = 'password';
+    const DBNAME = 'test_framework';
+    const DBPATH='database_files/sample.db';
+}
